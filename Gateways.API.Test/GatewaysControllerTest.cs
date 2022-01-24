52cf674d86c4ec087855a443f8e27a45a713187b
Yasmany Reyes González <yasma390@gmail.com>
2022-01-24 13:41:02 -0500

    Update Unit test.

--- Gateways.API.Test/GatewaysControllerTest.cs
+++ Gateways.API.Test/GatewaysControllerTest.cs
@@ -1,6 +1,8 @@
 ﻿using Gateways.Domaine;
 using Gateways.Persistence;
 using Gateways.Persistence.Repository;
+using Gateways.Persistence.Repository.Interface;
+using Gateways.Persistence.UnitOfWork;
 using Gateways.Persistence.UnitOfWork.Interface;
 using Microsoft.EntityFrameworkCore;
 using Moq;
@@ -15,13 +17,19 @@ namespace Gateways.API.Test
 {
     public class GatewaysControllerTest
     {
-        private DbContextOptions<GatewaysDbContext> _options;
+        DbContextOptions<GatewaysDbContext> _options;
+        GatewaysDbContext _context;
 
         [SetUp]
         public void Setup()
         {
             //Create in Memory Database
-            InitDbTest();
+            var dbName = $"GayewayssDb_{DateTime.Now.ToFileTimeUtc()}";
+            _options = new DbContextOptionsBuilder<GatewaysDbContext>()
+            .UseInMemoryDatabase(databaseName: dbName)
+            .Options;
+
+            _context = new GatewaysDbContext(_options);
         }
 
         [Test]
@@ -30,53 +38,37 @@ namespace Gateways.API.Test
             var repository = await CreateRepositoryAsync();
 
             // Act
-            var authorList = await repository.GetAllAsync();
+            var gateways = await repository.GetAllAsync();
 
             // Assert
-            Assert.AreEqual(3, authorList.Count);
+            Assert.AreEqual(3, gateways.Count);
         }
 
         [Test]
-        public void GetGatewayByIdTest()
-        {
-            // Use a clean instance of the context to run the test
-            using (var context = new GatewaysDbContext(_options))
-            {
-                var gatewayRepository = new GatewayRepository(context);
-                var gateway = gatewayRepository.GetbyIdAsync(1);
-
-                Assert.AreEqual("Huawei", gateway.Result.Name);
-                Assert.AreEqual("192.168.152.1", gateway.Result.IPAddress);
-                Assert.AreEqual("fg45s-586ed-45ert-4523d", gateway.Result.SerialNumber);
-            }
-        }
-
-        [Test]
-        public void GetByIdAsync_Returns_Gateway()
-        {
-            // Use a clean instance of the context to run the test
-            using (var context = new GatewaysDbContext(_options))
-            {
-                var gatewayRepository = new GatewayRepository(context);
-                var gateway = gatewayRepository.GetbyIdAsync(1).Result;
-
-                //Assert
-                Assert.NotNull(gateway);
-                Assert.IsAssignableFrom<Gateway>(gateway);
-            }
-        }
-
-        public async Task GetAuthorByIdAsync_Success_Test()
+        public async Task GetGatewayByIdAsync_Success_Test()
         {
             var repository = await CreateRepositoryAsync();
 
             // Act
-            var author = await repository.GetbyIdAsync(1);
+            var gateway = await repository.GetbyIdAsync(1);
 
             // Assert
-            Assert.NotNull(author);
-            Assert.AreEqual("Author_1", author.Name);
-            Assert.AreEqual(2, author.PeripheralDevices.Count);
+            Assert.NotNull(gateway);
+            Assert.AreEqual("Gateway_1", gateway.Name);
+            Assert.AreEqual(2, gateway.PeripheralDevices.Count);
+        }
+                
+        [Test]
+        public async Task DeleteAsync_Success_Test()
+        {
+            var repository = await CreateRepositoryAsync();
+
+            // Act
+            await repository.DeleteAsync(2);
+            await _context.SaveChangesAsync();
+            // Assert
+            var gateways = await repository.GetAllAsync();
+            Assert.AreEqual(2, gateways.Count);
         }
 
         [Test]
@@ -87,113 +79,64 @@ namespace Gateways.API.Test
             // Act
             await repository.AddAsync(new Gateway()
             {
-                Name = "Some Author",
+                Name = "Some Gateway",
                 PeripheralDevices = new List<PeripheralDevice>()
-            {
-                new PeripheralDevice { Vendor = $"Vendor_90",
-                        Status = (DeviceStatus)1,
-                        Created = DateTime.Now }
-            }
+                {
+                    new PeripheralDevice {
+                        Vendor = $"Vendor_90",
+                        Created = DateTime.Now,
+                        Status = (DeviceStatus)1
+                    }
+                }
             });
-
+            await _context.SaveChangesAsync();
             // Assert
             var gateways = await repository.GetAllAsync();
             Assert.AreEqual(4, gateways.Count);
         }
 
-        [Test]
-        public async Task DeleteAsync_Success_Test()
-        {
-            var repository = await CreateRepositoryAsync();
-
-            // Act
-            await repository.DeleteAsync(3);
-
-            // Assert
-            var gateways = await repository.GetAllAsync();
-            Assert.AreEqual(2, gateways.Count);
-        }
 
 
         #region Help Methods
 
-        private void InitDbTest()
-        {
-            _options = new DbContextOptionsBuilder<GatewaysDbContext>()
-                .UseInMemoryDatabase(databaseName: "GatewaysTestDb")
-                .Options;
-
-            // Insert seed data into the database using one instance of the context
-            using (var context = new GatewaysDbContext(_options))
-            {
-                var gateways = new List<Gateway>
-                {
-                    new Gateway
-                    {
-                        Id= 1,
-                        Name = "Huawei",
-                        IPAddress="192.168.152.1",
-                        SerialNumber = "fg45s-586ed-45ert-4523d"
-                    },
-                    new Gateway
-                    {
-                        Id=2,
-                        Name = "Gigabit",
-                        IPAddress="10.0.12.1",
-                        SerialNumber = "fwerf3-e32d-e6rt7-y834t"
-                    },
-                    new Gateway
-                    {
-                        Id=3,
-                        Name = "Xiaomi",
-                        IPAddress="12.114.22.78",
-                        SerialNumber = "f2edrgt-67h2d-e5st7-45rtt"
-                    }
-                };
-                context.Gateways.AddRange(gateways);
-                context.SaveChanges();
-            }
-
-        }
-
         private async Task<GatewayRepository> CreateRepositoryAsync()
         {
-            var context = new GatewaysDbContext(_options);
-            await PopulateDataAsync(context);
-            return new GatewayRepository(context);
+            await PopulateDataAsync();
+            return new GatewayRepository(_context);
         }
 
-        private async Task PopulateDataAsync(GatewaysDbContext context)
+        private async Task PopulateDataAsync()
         {
             int index = 1;
 
             while (index <= 3)
             {
-                var author = new Gateway()
+                var gateway = new Gateway()
                 {
+                    IPAddress = $"127.0.0.{index}",
                     Name = $"Gateway_{index}",
                     PeripheralDevices = new List<PeripheralDevice>()
-                {
+                    {
                     new PeripheralDevice
                     {
-                        Vendor = $"Vendor_1{index+4}",
+                        Vendor = $"Gateway_Vendor_1{index}",
                         Status = (DeviceStatus)0,
                         Created = DateTime.Now
                     },
                     new PeripheralDevice
                     {
-                        Vendor = $"Vendor_{index+3}2",
+                        Vendor = $"Gateway_Vendor_2{index}",
                         Status = (DeviceStatus)1,
                         Created = DateTime.Now
                     },
-                }
+                    }
                 };
 
                 index++;
-                await context.Gateways.AddAsync(author);
+                await _context.Gateways.AddAsync(gateway);
             }
 
-            await context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
         }
 
         #endregion
